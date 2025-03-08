64b3329dac2f0a6f2fd83eee9f71cf7c56a884e4
khanhphu <nghkphung95@gmail.com>
2025-03-08 13:25:26 +0700

    factory pattern

--- Project8/Controllers/CartController.cs
+++ Project8/Controllers/CartController.cs
@@ -2,163 +2,161 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 using WebBanSach.Models.Data;
 using WebBanSach.Models.Process;
 using WebBanSach.Models;
-using System.Web.Script.Serialization;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Project8.Models.Data;
-using Project8.Models.Process;
 
 namespace WebBanSach.Controllers
 {
     public class CartController : Controller
     {
         //khởi tạo dữ liệu
-        BSDBContext db = new BSDBContext();
-
-        //tạo 1 chuỗi hằng để gán session
-        private const string CartSession = "CartSession";
-
-        // GET: Cart/ : trang giỏ hàng
-        public ActionResult Index()
+        private readonly BSDBContext db;
+        private readonly IOrderFactory _orderFactory;
+        //public CartController()
+        //{
+        //    db = new BSDBContext();
+        //}
+        public CartController(BSDBContext db, IOrderFactory orderFactory)
         {
-            var cart = Session[CartSession];
-            var list = new List<CartModel>();
-            var sl = 0;
-            decimal? total = 0;
-            if (cart != null)
-            {
-                list = (List<CartModel>)cart;
-                sl = list.Sum(x => x.Quantity);
-                total = list.Sum(x => x.Total);
-            }
-            ViewBag.Quantity = sl;
-            ViewBag.Total = total;
-            return View(list);
+            this.db = db;
+            this._orderFactory = orderFactory;
         }
 
-        //GET : /Cart/CartHeader : đếm sổ sản phẩm trong giỏ hàng
-        //PartialView : CartHeader
+
+
+        private const string CartSession = "CartSession";
+
+
+        // GET: /Cart/Index
+        public ActionResult Index()
+        {
+            var cart = GetCartFromSession();
+            ViewBag.Total = cart.Sum(item => item.sach.GiaBan.GetValueOrDefault(0) * item.Quantity);
+            return View(cart);
+        }
+
+        // POST: /Cart/Add
+        [HttpPost]
+        public JsonResult Add(int id, int quantity = 1)
+        {
+            var book = db.Saches.Find(id);
+            if (book == null)
+            {
+                return Json(new { success = false, message = "Sách không tồn tại" });
+            }
+
+            var cart = GetCartFromSession();
+            var cartItem = cart.FirstOrDefault(item => item.sach.MaSach == id);
+
+            if (cartItem != null)
+            {
+                cartItem.Quantity += quantity;
+            }
+            else
+            {
+                cart.Add(new CartModel { sach = book, Quantity = quantity });
+            }
+
+            SaveCartToSession(cart);
+            var total = cart.Sum(item => item.sach.GiaBan.GetValueOrDefault(0) * item.Quantity);
+            return Json(new { success = true, total = total });
+        }
+
+        // POST: /Cart/Update
+        [HttpPost]
+        public JsonResult Update(int id, int quantity)
+        {
+            var cart = GetCartFromSession();
+            var cartItem = cart.FirstOrDefault(item => item.sach.MaSach == id);
+
+            if (cartItem == null)
+            {
+                return Json(new { success = false, message = "Sản phẩm không có trong giỏ hàng" });
+            }
+
+            if (quantity <= 0)
+            {
+                cart.Remove(cartItem); // Remove if quantity is 0 or less
+            }
+            else
+            {
+                cartItem.Quantity = quantity; // Update quantity
+            }
+
+            SaveCartToSession(cart);
+            var total = cart.Sum(item => item.sach.GiaBan.GetValueOrDefault(0) * item.Quantity);
+            return Json(new { success = true, total = total, itemCount = cart.Count });
+        }
+
+        // POST: /Cart/Remove
+        [HttpPost]
+        public JsonResult Remove(int id)
+        {
+            var cart = GetCartFromSession();
+            var cartItem = cart.FirstOrDefault(item => item.sach.MaSach == id);
+
+            if (cartItem == null)
+            {
+                return Json(new { success = false, message = "Sản phẩm không có trong giỏ hàng" });
+            }
+
+            cart.Remove(cartItem);
+            SaveCartToSession(cart);
+            var total = cart.Sum(item => item.sach.GiaBan.GetValueOrDefault(0) * item.Quantity);
+            return Json(new { success = true, total = total, itemCount = cart.Count });
+        }
+
+        // POST: /Cart/Clear
+        [HttpPost]
+        public JsonResult Clear()
+        {
+            Session["Cart"] = null; // Clear the session
+            return Json(new { success = true, total = 0 });
+        }
+
+        //// GET: /Cart/Payment
+        //public ActionResult Payment()
+        //{
+
+        //    // Add payment logic here
+        //    return View(cart);
+        //}
         public ActionResult CartHeader()
         {
-            var cart = Session[CartSession];
+            var cart = GetCartFromSession();
             var list = new List<CartModel>();
             if (cart != null)
             {
-                list = (List<CartModel>)cart;
+                list = cart;
             }
 
             return PartialView(list);
         }
 
-        //Xóa 1 sản phẩm trong giỏ hàng
-        public JsonResult Delete(int id)
+        // Helper methods for session handling
+        private List<CartModel> GetCartFromSession()
         {
-            var sessionCart = (List<CartModel>)Session[CartSession];
-            //xóa những giá trị mà có mã sách giống với id
-            sessionCart.RemoveAll(x => x.sach.MaSach == id);
-            //gán lại giá trị cho session
-            Session[CartSession] = sessionCart;
-
-            return Json(new
+            var cartJson = Session["Cart"] as string;
+            if (string.IsNullOrEmpty(cartJson))
             {
-                status = true
+                return new List<CartModel>();
+            }
+            return JsonConvert.DeserializeObject<List<CartModel>>(cartJson);
+        }
+
+        private void SaveCartToSession(List<CartModel> cart)
+        {
+            Session["Cart"] = JsonConvert.SerializeObject(cart, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
             });
         }
 
-        //Xóa tất cả các sản phẩm trong giỏ hàng
-        public JsonResult DeleteAll()
-        {
-            Session[CartSession] = null;
-            return Json(new
-            {
-                status = true
-            });
-        }
 
-        //Cập nhật giỏ hàng
-        public JsonResult Update(string cartModel)
-        {
-            //tạo 1 đối tượng dạng json
-            var jsonCart = new JavaScriptSerializer().Deserialize<List<CartModel>>(cartModel);
-
-            //ép kiểu từ session
-            var sessionCart = (List<CartModel>)Session[CartSession];
-
-            foreach (var item in sessionCart)
-            {
-                var jsonItem = jsonCart.Single(x => x.sach.MaSach == item.sach.MaSach);
-                if (jsonItem != null)
-                {
-                    item.Quantity = jsonItem.Quantity;
-                }
-            }
-            //cập nhật lại session
-            Session[CartSession] = sessionCart;
-
-            return Json(new
-            {
-                status = true
-            });
-        }
-
-        //GET : /Cart/AddItem/?id=?&quantity=1 : thêm sản phẩm vào giỏ hàng
-        public ActionResult AddItem(int id, int quantity)
-        {
-            //lấy mã sách và gán đối tượng
-            var sach = new AdminProcess().GetIdBook(id);
-
-            //lấy giỏ hàng từ session
-            var cart = Session[CartSession];
-
-            //nếu đã có sản phẩm trong giỏ hàng
-            if (cart != null)
-            {
-                var list = (List<CartModel>)cart;
-                if (list.Exists(x => x.sach.MaSach == id))
-                {
-
-                    foreach (var item in list)
-                    {
-                        if (item.sach.MaSach == id)
-                        {
-                            item.Quantity += quantity;
-                        }
-                    }
-                }
-                else
-                {
-                    //tạo mới đối tượng cart item
-                    var item = new CartModel();
-                    item.sach = sach;
-                    item.Quantity = quantity;
-                    list.Add(item);
-                }
-
-                //Gán vào session
-                Session[CartSession] = list;
-            }
-            else
-            {
-                //tạo mới giỏ hàng
-                var item = new CartModel();
-                item.sach = sach;
-                item.Quantity = quantity;
-                var list = new List<CartModel>();
-                list.Add(item);
-
-                //gán vào session
-                Session[CartSession] = list;
-            }
-
-            return RedirectToAction("Index");
-        }
-
-        //Thông tin khách hàng
         [HttpGet]
         [ChildActionOnly]
         public PartialViewResult UserInfo()
@@ -178,6 +176,17 @@ namespace WebBanSach.Controllers
             return PartialView();
         }
 
+
+
+        private int TaoMaDDH()
+        {
+            int r = (from ddh in db.DonDatHangs orderby ddh.MaDDH descending select ddh.MaDDH).First();
+            return r + 1;
+        }
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
         [HttpGet]
         public ActionResult Payment()
         {
@@ -188,70 +197,57 @@ namespace WebBanSach.Controllers
             }
             else
             {
-                var cart = Session[CartSession];
-                var list = new List<CartModel>();
-                var sl = 0;
-                decimal? total = 0;
-                if (cart != null)
+                var cart = GetCartFromSession();
+                if (cart.Count == 0)
                 {
-                    list = (List<CartModel>)cart;
-                    sl = list.Sum(x => x.Quantity);
-                    total = list.Sum(x => x.Total);
+                    return RedirectToAction("Index");
                 }
+
+                var list = cart; // Already a List<CartModel>
+                var sl = list.Sum(x => x.Quantity);
+                decimal? total = list.Sum(x => x.Total);
                 ViewBag.Quantity = sl;
                 ViewBag.Total = total;
                 return View(list);
             }
         }
-
-        private int TaoMaDDH()
-        {
-            int r = (from ddh in db.DonDatHangs orderby ddh.MaDDH descending select ddh.MaDDH).First();
-            return r + 1;
-        }
-
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         [HttpPost]
-        public ActionResult Payment(int MaKH, FormCollection f)
+        public async Task<ActionResult> Payment(FormCollection f)
         {
-            var order = new DonDatHang();
-            order.NgayDat = DateTime.Now;
-            order.NgayGiao = DateTime.Now.AddDays(3);
-            order.TinhTrang = true; //đã thanh toán
-            order.MaKH = MaKH;
-            order.MaDDH = TaoMaDDH();
+            var userName = Session["User"];
+            var user = db.KhachHangs.SingleOrDefault(x => x.TaiKhoan == userName);
+            int MaKH = user.MaKH;
+            var cart = GetCartFromSession();
+            if (cart == null || !cart.Any())
+            {
+                return RedirectToAction("Index"); // Cart empty, redirect to cart view
+            }
 
             try
             {
-                //thêm dữ liệu vào đơn đặt hàng
-                order.ThanhToan = 1;
-                var result1 = new OrderProcess().Insert(order);
-                var cart = (List<CartModel>)Session[CartSession];
-                var result2 = new OderDetailProcess();
-                decimal? total = 0;
-                foreach (var item in cart)
+               
+                // Create order using factory
+                var order = await _orderFactory.CreateOrderAsync(cart, MaKH); // Correct usage
+                order.ThanhToan = 1; // Paid (adjust based on your logic)                                                                          // MaDDH is auto-incremented by DB, no need for TaoMaDDH()
+                // Update stock
+                foreach (var item in order.ChiTietDDHs)
                 {
-                    var orderDetail = new ChiTietDDH();
-                    orderDetail.MaSach = item.sach.MaSach;
-                    orderDetail.MaDDH = result1;
-                    orderDetail.SoLuong = item.Quantity;
-                    orderDetail.DonGia = item.sach.GiaBan;
-                    result2.Insert(orderDetail);
-
-                    total = cart.Sum(x => x.Total);
-
+                    var book = db.Saches.Find(item.MaSach);
+                    book.SoLuongTon -= item.SoLuong;
                 }
 
-                Session[CartSession] = null;
+                // Save to database
+                db.DonDatHangs.Add(order);
+                db.SaveChanges();
+
+                Session[CartSession] = null; // Clear cart
                 return Redirect("/Cart/Success");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                // throw;
+                log.Error("Lỗi khi thanh toán: ", ex);
                 return Redirect("/Cart/Error");
             }
-
-            return new EmptyResult();
 
         }
         public ActionResult Success()
@@ -269,7 +265,7 @@ namespace WebBanSach.Controllers
             List<DonDatHang> donDatHang = db.DonDatHangs.Where(p => p.MaKH == UserController.khachhangstatic.MaKH).ToList();
             return View(donDatHang);
         }
-        
+
         public ActionResult TrackingOderDetails(int id)
         {
             var result = new OderDetailProcess().ListDetail(id);
---
+++ Project8/Controllers/CheckoutController.cs
@@ -0,0 +1,66 @@
+﻿using Project8.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebBanSach.Models;
+using WebBanSach.Models.Data;
+
+namespace Project8.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly BSDBContext _context;
+        private readonly IOrderFactory _orderFactory;
+
+        public CheckoutController(BSDBContext context, IOrderFactory orderFactory)
+        {
+            _context = context;
+            _orderFactory = orderFactory;
+        }
+        [HttpGet]
+        public ActionResult Checkout()
+        {
+            var cart = Session["Cart"] as List<CartModel> ?? new List<CartModel>();
+            return View(cart);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Checkout(int cusId, List<CartModel> cartModels)
+        {
+            if (!ModelState.IsValid || cartModels == null || !cartModels.Any())
+            {
+                ModelState.AddModelError("", "Gio hang trong hoac khong hop le!");
+                return View(cartModels);
+            }
+            try
+            {
+                var order = _orderFactory.CreateOrderAsync(cartModels, cusId).Result; // Synchronous for simplicity
+
+                foreach (var item in order.ChiTietDDHs)
+                {
+                    var book = _context.Saches.Find(item.MaSach);
+                    book.SoLuongTon -= item.SoLuong;
+                }
+
+                _context.DonDatHangs.Add(order);
+                _context.SaveChanges();
+
+                Session["Cart"] = new List<CartModel>(); // Clear cart after success
+                return RedirectToAction("OrderConfirmation", new { orderId = order.MaDDH });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(cartModels);
+            }
+        }
+        [HttpGet]
+        public ActionResult OrderConfirmation(int orderId)
+        {
+            return View(orderId);
+        }
+    }
+}
