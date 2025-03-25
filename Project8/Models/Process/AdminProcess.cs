e8f7c5d9f8b9089ac41192f01c2111d399ae814c
phk2754 <146325847+phk2754@users.noreply.github.com>
2025-03-26 04:41:34 +0700

    Update AdminProcess.cs

--- Project8/Models/Process/AdminProcess.cs
+++ Project8/Models/Process/AdminProcess.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using WebBanSach.Models.Data;
+using static WebBanSach.Models.Data.Sach;
 
 namespace WebBanSach.Models.Process
 {
