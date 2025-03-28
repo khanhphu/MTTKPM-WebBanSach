9fdc99b2e6c562869413ff0c1e2de9b067ce2f85
DuyKhaaa <duykhaduong2004@gmail.com>
2025-03-28 23:08:55 +0700

    Command

---
+++ Project8/Models/Data/Command/CancelOrderCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanSach.Models.Data.Command
+{
+    public class CancelOrderCommand : ICommand
+    {
+        private readonly BSDBContext _context;
+        private readonly int _orderId;
+        private bool _previousStatus;
+
+        public CancelOrderCommand(BSDBContext context, int orderId)
+        {
+            _context = context;
+            _orderId = orderId;
+        }
+
+        public void Execute()
+        {
+            var order = _context.DonDatHangs.Find(_orderId);
+            if (order != null)
+            {
+                _previousStatus = order.TinhTrang; // Lưu trạng thái trước đó
+                order.TinhTrang = false; // 0 - Đã hủy
+                _context.SaveChanges();
+            }
+        }
+
+        public void Undo()
+        {
+            var order = _context.DonDatHangs.Find(_orderId);
+            if (order != null)
+            {
+                order.TinhTrang = _previousStatus; // Khôi phục trạng thái trước đó
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Project8/Models/Data/Command/CommandManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanSach.Models.Data.Command
+{
+    public class CommandManager
+    {
+        private readonly Stack<ICommand> _commands = new Stack<ICommand>();
+
+        public void ExecuteCommand(ICommand command)
+        {
+            command.Execute();
+            _commands.Push(command); // Lưu lệnh vào stack
+        }
+
+        public void Undo()
+        {
+            if (_commands.Any())
+            {
+                var command = _commands.Pop(); // Lấy lệnh cuối cùng
+                command.Undo(); // Thực hiện hoàn tác
+            }
+        }
+    }
+}
