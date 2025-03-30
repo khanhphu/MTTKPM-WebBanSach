3c65993123d4891ef1a446d83644e7cb9279345d
phk2754 <phamhongkhai2754@gmail.com>
2025-03-31 00:11:07 +0700

    Repository

---
+++ Project8/Models/Repository/ITacGiaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebBanSach.Models.Data;
+
+namespace WebBanSach.Models.Repository
+{
+    public interface ITacGiaRepository
+    {
+        List<TacGia> GetAll();
+        TacGia GetById(int id);
+        int Add(TacGia entity);
+        int Update(TacGia entity);
+        bool Delete(int id);
+    }
+}
---
+++ Project8/Models/Repository/TacGiaRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebBanSach.Models.Data;
+using System.Data.Entity;
+
+namespace WebBanSach.Models.Repository
+{
+    public class TacGiaRepository : ITacGiaRepository
+    {
+        private readonly BSDBContext db;
+
+        public TacGiaRepository()
+        {
+            db = new BSDBContext(); // Khởi tạo trực tiếp BSDBContext
+        }
+
+        public List<TacGia> GetAll()
+        {
+            return db.TacGias.OrderBy(x => x.MaTG).ToList();
+        }
+
+        public TacGia GetById(int id)
+        {
+            return db.TacGias.Find(id);
+        }
+
+        public int Add(TacGia entity)
+        {
+            db.TacGias.Add(entity);
+            db.SaveChanges();
+            return entity.MaTG;
+        }
+
+        public int Update(TacGia entity)
+        {
+            var tg = db.TacGias.Find(entity.MaTG);
+            if (tg == null) return 0;
+
+            tg.TenTG = entity.TenTG;
+            tg.QueQuan = entity.QueQuan;
+            tg.NgaySinh = entity.NgaySinh;
+            tg.NgayMat = entity.NgayMat;
+            tg.TieuSu = entity.TieuSu;
+            db.SaveChanges();
+            return 1;
+        }
+
+        public bool Delete(int id)
+        {
+            var tg = db.TacGias.Find(id);
+            if (tg == null) return false;
+
+            db.TacGias.Remove(tg);
+            db.SaveChanges();
+            return true;
+        }
+    }
+}
